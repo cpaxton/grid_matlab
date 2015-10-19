3bcf636476fad0434296ec8042223898be410e08
Christopher Paxton <cpaxton3@jhu.edu>
2015-10-19 16:44:46 -0400

    adding new data; modified gate dist

--- init.m
+++ init.m
@@ -4,7 +4,7 @@ clear all;
 startup
 
 %% load data
-load('data/2015_09_09.mat');
+load('data/current.mat');
 
 %% set up other variables and settings         
 gmmColors = [1.0 0.0 0.0 1.0 0.0 1.0;
@@ -12,13 +12,13 @@ gmmColors = [1.0 0.0 0.0 1.0 0.0 1.0;
              0.0 0.0 1.0 1.0 1.0 0.0];
 colors = 'rgbmcy';
 markers = '......';
-primitive_names = {'Approach','Pass-Through','Connect','Pass-Through-To-Exit','Exit'};
-APPROACH = 1;
+primitive_names = {'Approach','Pass-Through','Connect','Exit'};
+APPROACH = 4;
 PASS = 2;
 CONNECT = 3;
-PASS_EXIT = 4;
-EXIT = 5;
-IN_EXIT = 6;
+%PASS_EXIT = 4;
+EXIT = 1;
+AT_EXIT = 5;
 
 %% show current
 if ~HOLD_OUT
