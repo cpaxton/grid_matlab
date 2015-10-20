d758b6af6093b9a1dca349045b8e3accd8136f28
Christopher Paxton <cpaxton3@jhu.edu>
2015-10-20 13:34:28 -0400

    re-added control for number of steps

--- motion/prob_planning.m
+++ motion/prob_planning.m
@@ -5,12 +5,12 @@ function [ traj, Z ] = prob_planning( x0, model, next_model, local_env, next_env
 %   env is a representation of the local environment
 %   Z is the initial distribution we will refine
 
-
+SHOW_FIGURES = false;
 STEP_SIZE = 0.6;
 N_SAMPLES = 50;
 N_ITER = 20;
 N_PRIMITIVES = 4;
-N_Z_DIM = 2*N_PRIMITIVES;
+N_Z_DIM = 3*N_PRIMITIVES;
 N_STEPS = 10;
 N_GEN_SAMPLES = 50*N_SAMPLES;
 
@@ -33,12 +33,12 @@ if nargin < 7
     movement_guess = 15;
     N_STEPS = ceil(norm(xg) / N_PRIMITIVES / movement_guess);
     
-    mu = normrnd(1,0.1,N_Z_DIM,1).*repmat([0;movement_guess],N_PRIMITIVES,1);
+    mu = normrnd(1,0.1,N_Z_DIM,1).*repmat([0;movement_guess;N_STEPS],N_PRIMITIVES,1);
     %cv = 0.01*cov([trials{1}{1}.rotation;trials{1}{1}.movement]');
-    cv = [10 0; 0 5];
+    cv = [10 0 0; 0 5 0; 0 0 1];
     sigma = eye(N_Z_DIM);
-    for i=1:2:N_Z_DIM
-        sigma(i:(i+1),i:(i+1)) = cv;
+    for i=1:3:N_Z_DIM
+        sigma(i:(i+2),i:(i+2)) = cv;
     end
     Z  = struct('mu',mu,'sigma',sigma);
 end
@@ -51,9 +51,16 @@ while iter < N_ITER
     samples = mvnsample(Z.mu,Z.sigma,N_GEN_SAMPLES);
     params = zeros(size(samples,1),N_SAMPLES);
     
-    %figure(1+iter);clf;hold on;
-    %draw_gates({local_env.gate});
-    %draw_surfaces(obstacles);
+    if SHOW_FIGURES
+        figure(1+iter);clf;hold on;
+        if model.use_gate
+            draw_gates({local_env.gate});
+        end
+        if model.use_prev_gate
+            draw_gates({local_env.prev_gate});
+        end 
+        draw_surfaces(obstacles);
+    end
     
     p = zeros(N_SAMPLES,1);
     pg = zeros(N_SAMPLES,1);
@@ -61,7 +68,8 @@ while iter < N_ITER
     sample = 0;
     for i = 1:N_GEN_SAMPLES
         p_z = log(mvnpdf(samples(:,i),Z.mu,Z.sigma));
-        traj = sample_seq(x0,samples(:,i),N_STEPS);
+        %traj = sample_seq(x0,samples(:,i),N_STEPS);
+        traj = sample_seq(x0,samples(:,i));
         
         if check_collisions(traj,obstacles) == 0
             
@@ -88,15 +96,17 @@ while iter < N_ITER
             p(sample) =  exp(p_action + p_goal);
             pg(sample) = exp(p_goal);
             
-            %if p_goal > -700
-            %    ok = true;
-            %end
+            if p_goal > -700
+                ok = true;
+            end
             
-            %r = p(sample) / 2 / max_p;
-            %if r > 1
-            %  r = 1;
-            %end
-            %plot(traj(1,:),traj(2,:),'color',[r,0,0]);
+            if SHOW_FIGURES
+                r = p(sample) / 2 / max_p;
+                if r > 1
+                    r = 1;
+                end
+                plot(traj(1,:),traj(2,:),'color',[r,0,0]);
+            end
             
             if sample == N_SAMPLES
                 break
@@ -140,6 +150,7 @@ while iter < N_ITER
     
     fprintf('... done iter %d. avg p = %f, avg obj = %f\n',iter,log(avg_p),log(avg_pg));
     iter = iter + 1;
+
 end
 
 [~,idx] = max(pg);
--- motion/sample_seq.m
+++ motion/sample_seq.m
@@ -1,10 +1,10 @@
-function traj = sample_seq(x0,z,t)
+function traj = sample_seq(x0,z)%,t)
     num = length(z);
     traj = [];
-    for i = 1:2:num
+    for i = 1:3:num
         u = z(i)/20;
         v = z(i+1);
-        %t = z(i+2);
+        t = z(i+2);
         seg = sample_primitive(x0,u,v,t);
         x0 = seg(1:5,end);
         traj = [traj seg(:,1:(end-1))];
