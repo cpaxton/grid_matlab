9c8bf2ea9253006303efc73b48304e8f07ef22da
Christopher Paxton <cpaxton3@jhu.edu>
2015-10-28 17:33:31 -0400

    deleted some confusing old test files; creating clustering features for different stuff; adding in a new test script for global tests

--- features/get_clustering_features.m
+++ features/get_clustering_features.m
@@ -1,4 +1,4 @@
-function [ features, names ] = get_clustering_features( traj, env )
+function [ features, names ] = get_clustering_features( trial, env )
 %GET_CLUSTERING_FEATURES Gets a subset of the features for automatically
 %clustering examples
 %   - This will compute the gate features for the closest gates, exit
@@ -9,10 +9,15 @@ function [ features, names ] = get_clustering_features( traj, env )
 %   some other way. The only way we actually get missing data is if we
 %   don't have an open and a closed gate.
 
+NGATE_FEATURES = 4; %6; %7;
+
+traj = [trial.x;trial.y;trial.w;trial.movement;trial.rotation];
 len = size(traj,2);
 
 gate = in_gates(traj(1,:),traj(2,:),env.gates);
 state = struct('gates_done',num2cell(gate<0),'next_gate',num2cell(ones(1,size(traj,2))));
+[goals, opts] = next_goal(trial,env);
+exit = [env.width;(env.height / 2);0];
 
 features = [];
 
@@ -20,9 +25,25 @@ for i = 1:len
     % take this slice of the example
     pt = traj(:,i);
     [p,state] = compute_predicates(pt,env,state(1));
-    f = p; % add in the predicates
-    it = in_tissue(pt,env.surfaces)
-    f = [f;it];
+    it = in_tissue(pt,env.surfaces);
+    ef = get_end_distance(pt,exit);
+    
+    goal = goals(i);
+    opt = opts(1,i);
+    prev_opt = opts(2,i);
+    
+    if goal > 1
+        pgd = get_gate_distances(pt,env.gates{goal-1}{prev_opt});
+    else
+        pgd = ones(NGATE_FEATURES,1) * nan;
+    end
+    if goal <= length(env.gates)
+        ngd = get_gate_distances(pt,env.gates{goal}{opt});
+    else
+        ngd = ones(NGATE_FEATURES,1) * nan;
+    end
+    
+    f = [p;it;ef;pgd;ngd];
     
     features = [features f];
 end
@@ -31,6 +52,9 @@ start = 1;
 names = cell(size(features,1),1);
 [names,start] = add_names(p,'predicates',start,names);
 [names,start] = add_names(it,'in_tissue',start,names);
+[names,start] = add_names(ef,'exit',start,names);
+[names,start] = add_names(pgd,'prev_gate',start,names);
+[names,start] = add_names(ngd,'next_gate',start,names);
 
 end
 
---
+++ tests/do_level_global_test.m
@@ -0,0 +1,92 @@
+
+%% solve level one thing at a time
+%env = envs{6};
+%env = envs{7};
+env = generate_environment(1920,1080,1,0);
+
+% plan ends with an "exit" action and goes to the final exit state
+plan = [1 5];
+ngates = length(env.gates);
+
+prev_gate = [];
+next_gate = [];
+
+if ngates > 0
+    
+    plan = [2 plan];
+    
+    prev_gate = ngates;
+    next_gate = ngates+1;
+    
+    if ngates > 1
+        for i = 1:(ngates-1)
+            plan = [2 3 plan];
+            ii = ngates - i;
+            prev_gate = [ii ii prev_gate];
+            next_gate = [ii+1 ii+1 next_gate ]
+        end
+    end
+    
+    % "approach" for the first gate
+    plan = [4 plan];
+    prev_gate = [0 prev_gate];
+    next_gate = [1 next_gate];
+end
+
+prev_gate = [prev_gate ngates ngates];
+next_gate = [next_gate ngates+1 ngates+1];
+
+x = [190,1000,0,0,0]';
+
+figure(1);clf;hold on;
+draw_environment(env);
+
+trajs = cell(length(plan),1);
+
+for i = 1:2 %length(plan)-1
+    
+    fprintf('==============\n');
+    fprintf('ACTION: %d, model=%d, gate=%d, prev_gate=%d\n',i,plan(i),next_gate(i),prev_gate(i));
+    
+    current = models{plan(i)};
+    goal = models{plan(i+1)};
+    
+    local_env = [];
+    local_env.exit = [env.width;env.height / 2; 0];
+    local_env.obstacles = {};
+    local_env.gates = env.gates;
+    if next_gate(i) <= length(env.gates)
+        local_env.gate = env.gates{next_gate(i)}{1};
+    end
+    if prev_gate(i) > 0
+        local_env.prev_gate = env.gates{prev_gate(i)}{1};
+    end
+    next_env = [];
+    next_env.exit = local_env.exit;
+    next_env.obstacles = {};
+    next_env.gates = env.gates;
+    if next_gate(i+1) <= length(env.gates)
+        next_env.gate = env.gates{next_gate(i+1)}{1};
+    end
+    if prev_gate(i+1) > 0
+        next_env.prev_gate = env.gates{prev_gate(i+1)}{1};
+    end
+    
+    traj = prob_planning(x,current,goal,local_env,next_env,env.surfaces);
+    plot(traj(1,:),traj(2,:),colors(plan(i)));
+    
+    x = traj(:,end);
+    
+    trajs{i} = traj;
+    
+end
+
+if bmm.k <= 3
+    fprintf('==============\n');
+    fprintf('ACTION: %d, model=%d, going to exit\n',i+1,plan(i+1));
+    
+    traj = prob_planning_no_goal(x,goal,next_env,env.surfaces);
+    plot(traj(1,:),traj(2,:));
+    
+    trajs{i+1} = traj;
+end
