ee5ae96c17fb18cc2dc95f0900f6519b8093bd9a
Chris Paxton <cpaxton3@jhu.edu>
2015-11-19 18:23:32 -0500

    adding in a unary model

---
+++ segmentation/unary.m
@@ -0,0 +1,12 @@
+function ll = unary( x, y, s, model )
+%UNARY Compute unary probability given the proposed model
+% This is computing p(class = y | x)
+%   x are the set of observations
+%   y is the class
+%   model is the model for the given class
+%   s is the sparsity term for the model
+
+
+
+end
+
--- tests/segmentation_test.m
+++ tests/segmentation_test.m
@@ -1,5 +1,19 @@
 
 
+NVIEWS = 3;
+NCLASSES = 4;
+LEN = 20;
+CLASS_LENS = [2,3,4,3];
+
 %% initialize variables
+x = cell(LEN,NVIEWS);
+y = rand(LEN,1);
+s = rand(NCLASSES,NVIEWS);
+models = cell(NCLASSES,NVIEWS);
 
+%% create fake data
 
+%% example
+i = 1;
+yi = 1;
+unary(x{yi,:},yi,s(yi,:),models{yi,:});
