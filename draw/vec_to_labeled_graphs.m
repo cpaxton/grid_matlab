5a6bd9d7b69cb803ebbdf53cb677bc01e1ed2a90
Chris Paxton <cpaxton3@jhu.edu>
2016-05-04 16:47:26 -0400

    adding new code to interpret long vector sequences of labels for training data

---
+++ draw/vec_to_labeled_graphs.m
@@ -0,0 +1,36 @@
+function all_labels = vec_to_labeled_graphs(trials,envs,vec,num_levels,colors)
+
+if nargin < 5
+   colors = 'rgbmcy'; 
+end
+
+all_labels = cell(num_levels,1);
+
+for i = 1:num_levels
+    all_labels{i} = cell(length(trials{i}),1);
+    for j = 1:length(trials{i})
+        all_labels{i}{j} = vec(1:length(trials{i}{j}.x));
+    end
+end
+
+for i = 1:num_levels
+   
+    figure(i); clf;
+    
+    draw_environment(envs{i});
+    
+    hold on;
+    
+    for j = 1:length(trials{i})
+       
+        for idx = 1:length(trials{i}{j}.x)
+           x = trials{i}{j}.x(idx);
+           y = trials{i}{j}.y(idx);
+           plot(x,y,['.' colors(all_labels{i}{j}(idx))]);
+        end
+        
+    end
+    
+end
+
+end
