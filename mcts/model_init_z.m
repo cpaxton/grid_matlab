6563b98dc1a9d916e83276ff1ccd530f0972c15a
Chris Paxton <paxton.chris@live.com>
2016-09-29 05:11:49 -0400

    init z for simple models

---
+++ mcts/model_init_z.m
@@ -0,0 +1,14 @@
+function model_init_z(model, num_steps, num_primitives, num_z_dim)
+    movement_guess = model.movement_mean;
+    rotation_guess = model.rotation_mean;
+    
+    assert (num_z_dim == 3);
+    
+    mu = normrnd(1,0.1,num_z_dim,1).*repmat([rotation_guess;movement_guess;num_steps],num_primitives,1);
+    cv = [model.movement_dev 0 0; 0 model.rotation_dev 0; 0 0 1];
+    sigma = eye(num_z_dim);
+    for i=1:3:num_z_dim
+        sigma(i:(i+2),i:(i+2)) = cv;
+    end
+    Z  = struct('mu',mu,'sigma',sigma);
+end
