0efc6d6b74d202d82cd41e830911758725410a8b
Chris Paxton <paxton.chris@live.com>
2016-09-29 05:57:00 -0400

    set up config in mcts node

--- mcts/model_init_z.m
+++ mcts/model_init_z.m
@@ -1,11 +1,11 @@
-function Z = model_init_z(model, num_steps, num_primitives, num_primitive_params)
+function Z = model_init_z(model, config)
     movement_guess = model.movement_mean;
     rotation_guess = model.rotation_mean;
     
-    assert (num_primitive_params == 3);
-    num_z_dim = num_primitives * num_primitive_params;
+    assert (config.num_primitive_params == 3);
+    num_z_dim = config.num_primitives * config.num_primitive_params;
     
-    mu = normrnd(1,0.1,num_z_dim,1).*repmat([rotation_guess;movement_guess;num_steps],num_primitives,1);
+    mu = normrnd(1,0.1,num_z_dim,1).*repmat([rotation_guess;movement_guess;num_steps],config.num_primitives,1);
     cv = [model.movement_dev 0 0; 0 model.rotation_dev 0; 0 0 1];
     sigma = eye(num_z_dim);
     for i=1:3:num_z_dim
