2a09e26918fe7a0f40701c100d615dad05ed4491
Chris Paxton <paxton.chris@live.com>
2016-10-10 04:49:53 -0400

    add extract and search to mcts

---
+++ mcts/mcts_extract.m
@@ -0,0 +1,4 @@
+function traj = mcts_extract(root)
+% MCTS_EXTRACT get best MCTS path through the tree
+
+end
---
+++ mcts/mcts_search.m
@@ -0,0 +1,8 @@
+function [ root ] = mcts_search( root )
+%MCTS_SEARCH run MCTS search through the tree 
+%   Uses the MctsNode objects, despite not being member functions.
+%   At each iteration, select and rollout down the tree.
+%   During backup, add extra options to tree.
+
+end
+
