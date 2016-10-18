ba8c988218fe1b7ff54957679d75ca561965d3ed
Chris Paxton <cpaxton3@jhu.edu>
2016-10-18 18:23:29 -0400

    adding new score

---
+++ mcts/mcts_utils/uct_compute_score.m
@@ -0,0 +1,4 @@
+function score = uct_compute_score(sum, nb, t, C)
+    %score = (sum / (nb + 1)) + (C * sqrt(log(t)/(nb + 1)));
+    score = (sum / (nb + 1)) * (1 + (C * sqrt(log(t)/(nb + 1))));
+end
