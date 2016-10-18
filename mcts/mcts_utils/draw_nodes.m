3ac7be76d7b3eab143e07e051c51ea0f1e44ebba
Chris Paxton <cpaxton3@jhu.edu>
2016-10-18 15:00:37 -0400

    adding draw_nodes

---
+++ mcts/mcts_utils/draw_nodes.m
@@ -0,0 +1,10 @@
+function draw_nodes(nodes)
+
+for i = 1:length(nodes)
+    for j = 1:min(1,length(nodes(i).trajs))
+       plot(nodes(i).trajs{j}(1,:),nodes(i).trajs{j}(2,:),...
+           'color',nodes(i).models{nodes(i).action_idx }.color);
+    end
+end
+
+end
