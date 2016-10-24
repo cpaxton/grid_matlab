0a022ba5805d7a95a977e527d61a277d1505d296
Chris Paxton <cpaxton3@jhu.edu>
2016-10-24 16:35:45 -0400

    modifications -- fixing log and making sure planning seems to work

--- mcts/pw/pw_compute_score.m
+++ mcts/pw/pw_compute_score.m
@@ -1,7 +1,7 @@
 function score = pw_compute_score(sum, nb, p, t, C, alpha)
     k = ceil(C * t^alpha);
-    fprintf('%f %f/%f\n',k,nb,t);
-    fprintf(' -- %f\n',sqrt(log(t)/(nb + 1)));
+    %fprintf('%f %f/%f\n',k,nb,t);
+    %fprintf(' -- %f\n',sqrt(log(t)/(nb + 1)));
     score = (sum / (nb + 1)) + (k * sqrt(log(t)/(nb + 1)));
     %score = (sum / (nb + 1)) * (1 + (k * sqrt(log(t)/(nb + 1))));
 end
