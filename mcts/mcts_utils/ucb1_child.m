42e37ea6a973cd846d757aec4f345c8d7c5a6ca0
Chris Paxton <cpaxton3@jhu.edu>
2016-10-24 14:30:29 -0400

    add new script: choose child based on score

---
+++ mcts/mcts_utils/ucb1_child.m
@@ -0,0 +1,10 @@
+function child = ucb1_child(nodes, curr_node)
+% Return a random child node (index in nodes)
+    len = length(nodes(curr_node).children);
+    assert(len > 0);
+    child = 1;
+    score = 0;
+    for i = 1:len
+        
+    end
+end
