e911b1b4c7889491e689e51c388c2c30d8be3107
Chris Paxton <paxton.chris@live.com>
2016-11-02 17:57:50 -0700

    apply GP to new sampled points

--- mcts/optimization/test_gp_update.m
+++ mcts/optimization/test_gp_update.m
@@ -2,8 +2,8 @@
 % create "dataset"
 
 [traj, nodes] = run_mcts_test(envs{4}, models);
-x = nodes(2).traj_params(:,1:10)';
-y = log(nodes(2).traj_p(1:10));
+x = nodes(2).traj_params';
+y = log(nodes(2).traj_p);
 min_xs = nodes(2).Z.mu(1) - 2 * nodes(2).Z.sigma(1,1);
 max_xs = nodes(2).Z.mu(1) + 2 * nodes(2).Z.sigma(1,1);
 xs = nodes(2).traj_params'; %linspace(min_xs, max_xs, 101)';                  % 61 test inputs
@@ -17,14 +17,16 @@ hyp = struct('mean', [], 'cov', [0 0], 'lik', -1);
 % new params
 hyp2 = minimize(hyp, @gp, -100, @infGaussLik, meanfunc, covfunc, likfunc, x, y);
 
+Z = nodes(2).Z;
 %% Run for N iterations
-
-% inference
-
-figure(2);
-[mu s2] = gp(hyp2, @infGaussLik, meanfunc, covfunc, likfunc, x, y, xs);
-plot_gp(mu, s2, x(:,1), y, xs(:,1));
-
-for i = 1:1
-    
+% GP/cross entropy optimization
+N_ITER = 1;
+N_GEN_SAMPLES = 50;
+for i = 1:N_ITER
+    samples = mvnsample(Z.mu,Z.sigma,N_GEN_SAMPLES);
+    [p, sp] = gp(hyp2, @infGaussLik, meanfunc, covfunc, likfunc, x, y, xs);
 end
+
+x0 = [190; 1000; 0; 0; 0];
+[pmax, idx] = max(p);
+sample_seq(x0,samples(:,idx));
