5468b693128c49ac2aa975c1d1cb7e4f3e73d14b
Chris Paxton <paxton.chris@live.com>
2016-11-04 18:51:49 -0700

    add "initialize tree" function

---
+++ mcts/mcts_utils/initialize_tree.m
@@ -0,0 +1,9 @@
+function [ nodes ] = initialize_tree(nodes, x0, w0, config)
+% INITIALIZE_TREE
+%   Descend through the tree
+%   Draw samples at each "level" until we have reached the end
+
+%% Start at the root node (idx = 1)
+% sample children at random according to transition probabilities
+
+end
