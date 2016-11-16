1a653e0ce72eefcc49f5c95e572255bc9c5c5c55
Chris Paxton <paxton.chris@live.com>
2016-11-15 18:24:38 -0800

    backup tree

--- mcts/mcts_utils/update_tree.m
+++ mcts/mcts_utils/update_tree.m
@@ -1,12 +1,27 @@
-function nodes = update_tree(nodes)
+function nodes = backup_tree_after_initialization(nodes)
 % UPDATE_TREE
 %   go through the whole tree and update everything
 %   set the values of each branch correctly
-terminal_idx = [];
+cur_idx = [];
 for i = 1:length(nodes)
    if nodes(i).is_terminal
-       terminal_idx = [terminal_idx i];
+       cur_idx = [cur_idx i];
    end
+end
+
+parent_traj = [nodes(cur_idx).traj_parent_traj];
+parent_node = [nodes(cur_idx).traj_parent_node];
+unique_parents = unique(parent_node);
+
+while length(parent_node) > 0
+    for i = 1:length(unique_parents)
+        parent = unique_parents(i);
+        for j = 1:length(cur_idx)
+            idx = cur_idx(j);
+            trajs = nodes(idx).traj_parent_traj(nodes(idx).traj_parent_node == parent);
+        end
+    end
+    break
 end
 
 end
